6570679945455dabc25ae1c9691ac0995c19de40
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-04 15:02:38 +0200

    Create symlinks for BAMs

--- scripts/custom/create_symlinks.py
+++ scripts/custom/create_symlinks.py
@@ -7,13 +7,13 @@ import subprocess
 df = pd.read_csv("../../doc/sample_manifest.csv")
 data_path = Path('/home/gpfs/o_mykhaili/galushka/data')
 
-for row in df.iterrows():
+for idx, row in df.iterrows():
     bam_path = row['bam_path']
-    symlink_path = row['sample_id']
+    symlink_path = data_path/ f'{row['sample_id']}_30x.bam'
 
-    cmd = f'ln -s {bam_path} {data_path}'
-
-    subprocess.run(cmd,
-                   check=True,
-                   shell=True)
+    cmd = f'ln -s {bam_path} {str(symlink_path)}'
+    print(cmd)
+    # subprocess.run(cmd,
+    #                check=True,
+    #                shell=True)
 
