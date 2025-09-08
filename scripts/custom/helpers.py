18f6899a6b289b464e5f1fbb9fbf7f8cfa118502
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-08 11:12:12 +0200

    Trying to pull functions from other files

--- scripts/custom/helpers.py
+++ scripts/custom/helpers.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 
 def get_highest_cov_bam(wc, data_dir):
-    bams = list(Path(data_dir).glob(f"{wc.sample}_*.bam"))
+    bams = list(Path(data_dir).glob(f"{wc.sample}_*x.bam"))
     covs = [int(bam.stem.split("_")[-1]) for bam in bams]
     best_cov = max(covs)
-    return str(Path(data_dir) / f"{wc.sample}_{best_cov}.bam")
+    return str(Path(data_dir) / f"{wc.sample}_{best_cov}x.bam")
 
 def get_highest_cov_bai(wc, data_dir):
     return get_highest_cov_bam(wc, data_dir) + ".bai"
