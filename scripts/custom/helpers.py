563dd89ca6363cb1012ebdd716c328a6912aa681
o_mykhaili1 <matvii.mykhailichenko@medunigraz.at>
2025-09-08 10:45:42 +0200

    Trying to pull functions from other files

---
+++ scripts/custom/helpers.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+
+def get_highest_cov_bam(wc, data_dir):
+    bams = list(Path(data_dir).glob(f"{wc.sample}_*.bam"))
+    covs = [int(bam.stem.split("_")[-1]) for bam in bams]
+    best_cov = max(covs)
+    return str(Path(data_dir) / f"{wc.sample}_{best_cov}.bam")
+
+def get_highest_cov_bai(wc, data_dir="../data"):
+    return get_highest_cov_bam(wc, data_dir) + ".bai"
